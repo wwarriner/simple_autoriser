48da0869336471d5092684414746c51c05583433
UAB\wwarr <wwarr@uab.edu>
2018-11-05 13:08:00 -0600

    updated patch example so output is prettier

--- patch_example.m
+++ patch_example.m
@@ -2,19 +2,15 @@ function patch_example( stl, feeders )
 %% SETUP
 lightgray = [ 0.9 0.9 0.9 ];
 avoided_colors = [ 0 0 0; lightgray; 1 1 1 ];
-ph = patch( stl, 'facecolor', lightgray, 'edgealpha', 0.2 );
-ph.FaceColor = lightgray; ph.EdgeAlpha = 0.2;
 colors = distinguishable_colors( feeders.count, avoided_colors );
 fh = figure( 'color', 'w' );
 axh = axes( fh );
 
 %% DRAW
-patch( stl, 'facecolor', lightgray, 'edgealpha', 0.1 );
+patch( stl, 'facecolor', lightgray, 'edgealpha', 0 );
 hold( axh, 'on' );
 for i = 1 : feeders.count    
-    ph = patch( feeders.fvs{ i } );
-    ph.FaceColor = colors( i, : );
-    ph.EdgeAlpha = 0.1;
+    patch( feeders.fvs{ i }, 'facecolor', colors( i, : ), 'edgealpha', 0 );
 end
 
 %% ADJUST
@@ -23,5 +19,6 @@ axis( 'vis3d' );
 axis( 'off' );
 view( 45+90*2, 45 );
 camzoom( axh, 0.8 );
+camlight( axh, 'right' );
 
 end
